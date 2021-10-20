ee75be3f4297da46e0f684fce83ac290f8aa2dec
Fanny Fraiture <ffraiture.fanny@gmail.com>
2021-10-20 09:55:15 +0200

    ex 2

--- 10-asynchronous/02-get-comments/script.js
+++ 10-asynchronous/02-get-comments/script.js
@@ -9,11 +9,42 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(() => {
     
     // 1. on click on the button
+document.querySelector("#run").addEventListener("click", () => {
     // 2. call the getPosts function
+    window.lib.getPosts( (error, posts) => {
     //     3. For each item, calls the getComments function
+    posts.forEach(post => {
+        window.lib.getComments( posts.id, (err, comments) => {
     //         4. adds the comments obtained in a comments property of the item
+    posts.comment = comments
+        } )
+    });
     // 5. display the posts in the console
-})();
+    console.log(posts)
+        }
+    );
+});
+
+
+    /*(() => {
+        document.querySelector("#run").addEventListener("click", () => {
+
+            window.lib.getPosts(
+                (err, posts) => {
+                    posts.forEach(post => {
+                        window.lib.getComments(
+                            post.id, (err_, comments) => {
+                                post.comments = [];
+                                comments.forEach(
+                                    comment => post.comments.push(comment)
+                                );
+                            }
+                        );
+                        console.log(post);
+                    });
+                }
+            );
+        });
+    })();*/
