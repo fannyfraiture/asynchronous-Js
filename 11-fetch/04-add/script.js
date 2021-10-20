25c4c32a6148e7d6cfed0dfbc128d2106282674d
Fanny Fraiture <ffraiture.fanny@gmail.com>
2021-10-20 16:50:17 +0200

    correction

--- 11-fetch/04-add/script.js
+++ 11-fetch/04-add/script.js
@@ -9,27 +9,4 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(() => {
-    
-    const inputName = document.querySelector('#hero-name')
-    const inputAlterEgo = document.querySelector('#hero-alter-ego')
-    const inputPower = document.querySelector('#hero-powers')
-    
-    document.querySelector("#run").addEventListener("click", async () => {
-        let abilities = inputPower.value.split(' ')
-        await fetch("http://localhost:3000/heroes", {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify({
-                name: inputName.value,
-                alterEgo: inputAlterEgo.value,
-                abilities: abilities
-            })
-        });
-        const getAll = await fetch("http://localhost:3000/heroes")
-        console.log(await getAll.json())
-        
-    })
-})();
+
--- 11-fetch/05-delete/script.js
+++ 11-fetch/05-delete/script.js
@@ -8,14 +8,4 @@
 
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
-(() => {
-    const input = document.querySelector("#hero-id")
 
-    document.querySelector('#run').addEventListener("click", async () => {
-        const response = await fetch("http://localhost:3000/heroes/"+input.value, {
-            method: 'DELETE'
-        });
-        const data = await response.json()
-        console.log(data)
-    });
-})();
