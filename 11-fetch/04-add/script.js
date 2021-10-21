3b9029da13c389a960670f46bb27ea04842df5fd
Fanny Fraiture <ffraiture.fanny@gmail.com>
2021-10-21 10:23:47 +0200

    correction fab

--- 11-fetch/04-add/script.js
+++ 11-fetch/04-add/script.js
@@ -9,4 +9,34 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+(() => {
+    
+  const inputName = document.querySelector("#hero-name");
+  const inputAlterEgo = document.querySelector("#hero-alter-ego");
+  const inputPower = document.querySelector("#hero-powers");
 
+  document.querySelector("#run").addEventListener("click", async () => {
+    try {
+      let abilities = inputPower.value.split(" ");
+      const res = await fetch("http://localhost:3000/heroes", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: inputName.value,
+          alterEgo: inputAlterEgo.value,
+          abilities: abilities,
+        }),
+      });
+      if(!res.ok) throw new Error(res.statusText)
+      const getAll = await fetch("http://localhost:3000/heroes");
+      if(!res.ok) throw new Error(res.statusText)
+      success();
+      console.log(await getAll.json());
+    } catch (err) {
+        error()
+        console.error('Something went WRONG !',err);
+    }
+  });
+})();
